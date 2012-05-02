ee9cf2bcd0f774e480805b8711e6fcaa908e0c92
Daniel Simmons <dan@dans.im>
2012-05-02 17:53:23 +0100

    Move twitterbot.js to lib

--- worker.js
+++ worker.js
@@ -1,50 +1,8 @@
-var Twit = require('twit');
-var config = require('./config.js')
-var querystring = require('querystring');
+var config = require('./config.js');
+var TwitterBot = require('./lib/twitterbot.js');
 
-var twitter = new Twit(config.twitter_auth);
+// Create new instance of twitterbot
+var twitterbot = new TwitterBot(config);
 
-// Global variable
-var next_refresh_url = '';
-
-setInterval(function() {
-
-    var params = [];
-
-    // Strip leading '?' from refresh_url parameter so it can be parsed by the
-    // querystring module.
-    if (next_refresh_url.charAt(0) === '?') {
-        next_refresh_url = next_refresh_url.slice(1);
-    }
-
-    // Use search parameters specified in the refresh_url parameter of the
-    // previous API response (if there was one)
-    params = querystring.parse(next_refresh_url);
-
-    // Override the search parameter with our own search string
-    params['q'] = config.phrase;
-
-    twitter.get('search', params, function(err, data) {
-        // Update global variable next_refresh_url with the next refresh url
-        // as provided by twitter. This will ensure we perform the next search
-        // from the last tweet ID, to avoid parsing results we've seen before.
-        if ('refresh_url' in data) {
-            next_refresh_url = data.refresh_url;
-        }
-
-        for (var i = 0, l = data.results.length; i < l; i++) {
-            // Have to double check the phrase is actually in the tweet, because
-            // the search isn't phrase-based
-            if (data.results[i].text.indexOf(config.phrase) > 0) {
-                // Yes, it is! Construct reply
-                console.log('In reply to ' + data.results[i].from_user + ': ' + data.results[i].text);
-                var reply = {
-                    status: '@' + data.results[i].from_user + ' I think you mean: "bear with me".',
-                    in_reply_to_status_id: data.results[i].id,
-                }
-                console.log(reply);
-            }
-        }
-    });
-
-}, config.interval);
+// Start polling
+twitterbot.poll(config.interval);
